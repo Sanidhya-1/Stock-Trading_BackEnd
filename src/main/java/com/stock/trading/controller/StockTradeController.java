ecf91ddac3de2c29c89e055bbb0dc70dbc046bbd
Sanidhya kumar mishra <sanidhyakrmishra@gmail.com>
2022-10-13 21:50:52 +0530

    added the packeges and a controller mathod

---
+++ src/main/java/com/stock/trading/controller/StockTradeController.java
@@ -0,0 +1,30 @@
+package com.stock.trading.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+
+public class StockTradeController {
+	
+	@Autowired
+	private StockTradeService stockTradeService;
+	
+	@PostMapping(value = "/check/")
+	public ResponseEntity<?> checkIfAccountAttached(@RequestBody String email){
+		
+		boolean result = this.stockTradeService.getProfileAttribute(email);
+		
+		if(!result) {
+			return ResponseEntity.badRequest().body("Bank details is not attached for this relative");
+		}else {
+			return ResponseEntity.ok("success");
+		}
+		
+		
+		
+	}
+	
+	
+
+}
